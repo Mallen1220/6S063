cb84d111bfb84e7cef84c7e92d9ecf6e4ad317f7
Timothy Chong <ttlchong@mit.edu>
2017-12-13 23:18:17 -0500

    Refactored code and added comments

--- Final Project/Code/pyServer.py
+++ Final Project/Code/pyServer.py
@@ -7,99 +7,133 @@ import requests
 from pythonosc import dispatcher
 from pythonosc import osc_server
 
-# Nodejs Control Server
-host = 'http://localhost:3000'
-state_address = '/state/'
-reset_address = '/reset'
-
-# Calibration Variables
-is_callibrated = False
-startTime = 0
-cycle = 0
-totalCycles = 12 # 2 * number of different music and light states
-duration = 30.0 # seconds per cycle
-movingAverage = [[0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0]]
-movingTotal = [[0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0]]
-movingCount = [0,0,0,0]
-fConfidence = []
-rConfidence = []
-rollingMax = 100
-rollingAverage = [[[],[],[],[],[]], [[],[],[],[],[]], [[],[],[],[],[]], [[],[],[],[],[]]]
-# Generate rolling average data structure
-for node in range(4):
-    for band in range(5):
-        rollingAverage[node][band] = [0 for i in range(rollingMax)]
-rollingAverageVal = [[0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0]]
-rollingCount = [0,0,0,0]
-
-
-maxF = 0
-maxR = 0
-
-maxF_i = 0
-maxR_i = 0
-
-
-def parseBandPower(addr, *args):
-    # args = args[:5]
-    #print("BP: ",args)
-    node = args[0] - 1
-    if node > 3:
-        return
-    count = rollingCount[node]
-    for band in range(5):
-        oldValue = rollingAverage[node][band][count]
-        newValue = args[band + 1]
-        rollingAverageVal[node][band] = ((rollingAverageVal[node][band]*rollingMax) - oldValue + newValue)/float(rollingMax)
-        rollingAverage[node][band][count] = newValue
-    rollingCount[node] = (count + 1) % rollingMax
-    if not is_callibrated:
-        if cycle == 0 or cycle > totalCycles:
-            return
-        node = args[0] - 1
-        for band in range(5):
-            movingAverage[node][band] = (movingAverage[node][band]*movingCount[node] + args[band + 1])/float(movingCount[node]+1)
-            movingTotal[node][band] += args[band + 1]
-        movingCount[node] += 1
-
-#### Calculates FOCUS confidence value from BP array ####
-# Input: bpArray = [delta, theta, alpha, beta, gamma]
-# Output: confidenceValue = (0-1)
-def confidenceFocus(bpArray):
-    delta = bpArray[0]
-    theta = bpArray[1]
-    beta = bpArray[2]
-    multiplier = 1.0
-    if beta < theta:
-        #multiplier *= 0.8
-        multiplier *= beta/float(theta)
-    if beta < delta:
-        multiplier *= 0.4
-    # TODO: calibrate confidence calculator
-    return multiplier
-
-
-#### Calculates RELAX confidence value from BP array ####
-# Input: bpArray = [delta, theta, alpha, beta, gamma]
-# Output: confidenceValue = (0-1)
-def confidenceRelax(bpArray):
-    delta = bpArray[0]
-    theta = bpArray[1]
-    beta = bpArray[2]
-    multiplier = 1.0
-    if beta > theta:
-        #multiplier *= 0.8
-        multiplier *= theta/float(beta)
-    if beta > delta:
-        multiplier *= 0.4
-    # TODO: calibrate confidence calculator
-    return multiplier
-
-def resetState():
-    request = requests.get(host + reset_address)
-    return
-
 if __name__ == "__main__":
+
+    # Nodejs Control Server
+    host = 'http://localhost:3000'
+    state_address = '/state/'
+    reset_address = '/reset'
+
+    # Calibration Variables
+    is_callibrated = False
+    startTime = 0
+    cycle = 0
+    totalCycles = 12                                                                # 2 * number of different music and light states
+    duration = 30.0                                                                 # seconds per cycle
+
+    # Number of Nodes (Ganglion Board = 4)
+    num_nodes = 4
+    # Number of Bands (Theta, Delta, Alpha, Beta, Gamma = 5)
+    num_bands = 5
+    # Number of data points stored for calculating rolling average
+    num_rolling_dp = 100
+
+    # Initialize Data Structures
+    movingAverage = [[0 for i in range(num_bands)] for j in range(num_nodes)]       #[[0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0]]
+    movingTotal = [[0 for i in range(num_bands)] for j in range(num_nodes)]         #[[0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0]]
+    movingCount = [0 for i in range(num_nodes)]                                     #[0,0,0,0]
+    fConfidence = []
+    rConfidence = []
+    rollingDataSet = [[[0 for i in range(num_rolling_dp)] for j in range(num_bands)] for k in range(num_nodes)]# [[[],[],[],[],[]], [[],[],[],[],[]], [[],[],[],[],[]], [[],[],[],[],[]]]
+    rollingAverage = [[0 for i in range(num_bands)] for j in range(num_nodes)]   #[[0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0]]
+    rollingCount = [0 for i in range(num_nodes)]                                    #[0,0,0,0]
+
+    # MAX heuristic value for Focus & Relax
+    maxF = 0
+    maxR = 0
+    # Index of cycle with MAX heuristic value for Focus & Relax
+    maxF_i = 0
+    maxR_i = 0
+
+    #### Callback function for each incoming OSC packet containing Band Power data ####
+    def parseBandPower(addr, *args):
+        # args = args[:5]
+        #print("BP: ",args)
+        node = args[0] - 1
+        if node > 3:
+            return
+        count = rollingCount[node]
+        for band in range(num_bands):
+            oldValue = rollingDataSet[node][band][count]
+            newValue = args[band + 1]
+            rollingAverage[node][band] = ((rollingAverage[node][band]*num_rolling_dp) - oldValue + newValue)/float(num_rolling_dp)
+            rollingDataSet[node][band][count] = newValue
+        rollingCount[node] = (count + 1) % num_rolling_dp
+        if not is_callibrated:
+            if cycle == 0 or cycle > totalCycles:
+                return
+            node = args[0] - 1
+            for band in range(num_bands):
+                movingAverage[node][band] = (movingAverage[node][band]*movingCount[node] + args[band + 1])/float(movingCount[node]+1)
+                movingTotal[node][band] += args[band + 1]
+            movingCount[node] += 1
+
+    #### Calculates FOCUS confidence value from BP array ####
+    # Input: bpArray = [delta, theta, alpha, beta, gamma]
+    # Output: confidenceValue = (0-1)
+    def confidenceFocus(bpArray):
+        delta = bpArray[0]
+        theta = bpArray[1]
+        beta = bpArray[2]
+        multiplier = 1.0
+        if beta < theta:
+            multiplier *= beta/float(theta)
+        if beta < delta:
+            multiplier *= 0.4
+        return multiplier
+
+    #### Calculates RELAX confidence value from BP array ####
+    # Input: bpArray = [delta, theta, alpha, beta, gamma]
+    # Output: confidenceValue = (0-1)
+    def confidenceRelax(bpArray):
+        delta = bpArray[0]
+        theta = bpArray[1]
+        beta = bpArray[2]
+        multiplier = 1.0
+        if beta > theta:
+            multiplier *= theta/float(beta)
+        if beta > delta:
+            multiplier *= 0.4
+        return multiplier
+
+    def confidenceFromMovingAverage(confidenceFX):
+        confidence_val = 0
+        for node in range(num_nodes):
+            confidence_val += confidenceFX(movingAverage[node])
+        confidence_val /= float(num_nodes)
+        return confidence_val
+
+    def resetState():
+        request = requests.get(host + reset_address)
+        return
+
+    def resetConfidence():
+        global fConfidence
+        global rConfidence
+        fConfidence = []
+        rConfidence = []
+
+    def resetDataStructures():
+        resetMovingAverage()
+        resetRollingAverage()
+
+    def resetMovingAverage():
+        global movingAverage
+        global movingTotal
+        global movingCount
+        movingAverage = [[0 for i in range(num_bands)] for j in range(num_nodes)]
+        movingTotal = [[0 for i in range(num_bands)] for j in range(num_nodes)]
+        movingCount = [0 for i in range(num_nodes)]
+
+    def resetRollingAverage():
+        global rollingDataSet
+        global rollingAverage
+        global rollingCount
+        rollingDataSet = [[[0 for i in range(num_rolling_dp)] for j in range(num_bands)] for k in range(num_nodes)]
+        rollingAverage = [[0 for i in range(num_bands)] for j in range(num_nodes)]
+        rollingCount = [0 for i in range(num_nodes)]
+
+
     # Parse Arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("--ip",
@@ -119,18 +153,18 @@ if __name__ == "__main__":
     # Reset light and music
     resetState()
 
-    # Main Loop Variables
+    # Main Loop State
     inp = None
-
 
     while (True):
         if inp == "calibrate" or inp == "c":
             # Run calibration
             if cycle == 0:
                 # Set first cycles light and music state
+                resetConfidence()
                 cycle += 1
                 print("Start focused activity")
-                print("(press any button to continue)")
+                print("(press ENTER to continue)")
                 input()
                 request = requests.get(host + state_address + str(cycle))
                 is_callibrated = False
@@ -138,27 +172,20 @@ if __name__ == "__main__":
                 print("Cycle: " + str(cycle))
             if cycle <= totalCycles/2:
                 if startTime + duration < time.time():
-                    # Calculate final average number
-                    f_c = 0
-                    for node in range(4):
-                        f_c += confidenceFocus(movingAverage[node])
-                    f_c /= 4.0
+                    f_c = confidenceFromMovingAverage(confidenceFocus)
                     fConfidence.append(f_c)
 
-                    # Change to the next cycle and adapt light and music state
+                    # Move on to next Cycle: Reset Variables + Change light/music state
                     cycle += 1
                     if cycle == int(totalCycles/2) + 1:
                         # Reset lights and music before next set of cyles
                         requests.get(host + reset_address)
                         print("Switch to relaxed activity")
-                        print("(press anything to continue)")
+                        print("(press ENTER to continue)")
                         input()
                     
                     # Move to next cycle
-                    # Reset values
-                    movingAverage = [[0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0]]
-                    movingTotal = [[0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0]]
-                    movingCount = [0,0,0,0]
+                    resetMovingAverage()
 
                     request = requests.get(host + state_address + str(cycle%int(totalCycles/2)))
                     startTime = time.time()
@@ -168,21 +195,13 @@ if __name__ == "__main__":
                     continue
             elif cycle <= totalCycles:
                 if startTime + duration < time.time():
-                    # Calculate final average number
-                    r_c = 0
-                    for node in range(4):
-                        r_c += confidenceRelax(movingAverage[node])
-                    r_c /= 4.0
+                    r_c = confidenceFromMovingAverage(confidenceRelax)
                     rConfidence.append(r_c)
 
-                    # Change to the next cycle and adapt light and music state
+                    # Move on to next Cycle: Reset Variables + Change light/music state
                     cycle += 1
                     
-                    # Move to next cycle
-                    # Reset values
-                    movingAverage = [[0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0]]
-                    movingTotal = [[0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0]]
-                    movingCount = [0,0,0,0]
+                    resetMovingAverage()
 
                     request = requests.get(host + state_address + str(cycle%int(totalCycles/2)))
                     startTime = time.time()
@@ -217,6 +236,12 @@ if __name__ == "__main__":
             # Use the calibrated settings to make user focused
             request = requests.get(host + state_address + str(maxF_i))
             print("State Changed")
+            inp = None
+        elif inp == "d" or inp == "debug":
+            print("Focus Confidence – " + str(fConfidence))
+            print("Relax Confidence – " + str(rConfidence))
+            print("Max Focus Index – " + str(maxF_i))
+            print("Max Relax Index – " + str(maxR_i))
             inp = None
         elif inp == "q" or inp == "quit":
             print("Quitting application...")
